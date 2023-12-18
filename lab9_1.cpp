446d002c3d028a59b5b8ad473a960905fd4c0b44
Ping <supasin_bo@cmu.ac.th>
2023-12-18 21:33:26 +0700

    lab9_1.cpp complete

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,11 +2,11 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
 	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
-
+		cout << "Enter interest rate per year (%): ";
+		cout << "Enter amount you can pay per year: ";
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -21,13 +21,13 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
+	cout << setw(13) << left << 1;
 	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << 50.0; 
+	    cout << setw(13) << left << 1050.0;
+	    cout << setw(13) << left << 100.0;
+	    cout << setw(13) << left << 950.0;
+	    cout << "\n";
+	    	
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,14 @@
 #include<iostream>
 using namespace std;
-
-//Write the function printO() here
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
-}
+int printO(int n, int m){
+    if(n <= 0 || m <= 0){
+        cout << "Invalid input";
+        }
+    for(int x=0; x < n; x++){
+        for(int y = 0; y < m; y++){
+            cout << "O";
+            }
+        cout <<endl;    
+        }
+    return 0;
+    }
